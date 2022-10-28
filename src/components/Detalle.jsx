b3312d8f29c2d46f781b8e5166625efa9114fd3f
Juan Jimenez <juanjiimenez8@gmail.com>
2022-10-28 10:59:46 -0500

    modules

--- src/components/Detalle.jsx
+++ src/components/Detalle.jsx
@@ -1,147 +1,55 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Table, Button, Container, Modal,  ModalHeader,  ModalBody,  FormGroup,  ModalFooter } from "reactstrap";
-import { useSelector } from "react-redux";
-
-const data = [
-
-  ];
-
-class Detalle extends React.Component {
-    
-    
-    state = {
-        data: data,
-        modalActualizar: false,
-        modalInsertar: false,
-        form: {
-            id: "",
-            nombre: "",
-            apellido: "",
-        }
-    }
-
-    DummyView = () => {
-        const reducer = useSelector(state => state.registros);
-        console.log(reducer[0])
+import { Container, Table, Button } from "reactstrap";
+import { useSelector, useDispatch  } from "react-redux";
+import { deleteRegistro } from "../features/registros/registrosSlice";
 
 
-    }
+function Detalle() {
+    const registros = useSelector((state) => state.registros);
+    const dispatch = useDispatch();
 
-
-    handleChange = (e) => {
-        this.setState({
-            form: {
-                ...this.state.form,
-                [e.target.name]: e.target.value,
-            },
-        });
+    const handleDelete = (id) => {
+        dispatch(deleteRegistro(id));
+        console.log('eliminar' + id);
     };
 
-    mostrarModalActualizar = (dato) => {
-        this.setState({
-            form: dato,
-            modalActualizar: true,
-        });
-    };
+    return (
+        <>
+            <div className='my-2 text-center'>
+                <Container>
+                    <div className='my-5 text-center'>
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Nombre</th>
+                                    <th>Apellido</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
 
-    cerrarModalActualizar = () => {
-        this.setState({ modalActualizar: false });
-    };
-
-    editar = (dato) => {
-        var contador = 0;
-        var arreglo = this.state.data;
-        arreglo.map((registro) => {
-            if (dato.id === registro.id) {
-                arreglo[contador].nombre = dato.nombre;
-                arreglo[contador].apellido = dato.apellido;
-            }
-            contador++;
-        });
-        this.setState({ data: arreglo, modalActualizar: false });
-    };
-
-    eliminar = (dato) => {
-        var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento " + dato.id);
-        if (opcion === true) {
-            var contador = 0;
-            var arreglo = this.state.data;
-            arreglo.map((registro) => {
-                if (dato.id === registro.id) {
-                    arreglo.splice(contador, 1);
-                }
-                contador++;
-            });
-            this.setState({ data: arreglo, modalActualizar: false });
-        }
-    };
-
-    render() {
-
-        return (
-            <>
-                <this.DummyView/>
-                <div className='my-2 text-center'>
-                    <Container>
-                        <div className='my-5 text-center'>
-                            <Table>
-                                <thead>
-                                    <tr>
-                                        <th>Id</th>
-                                        <th>Nombre</th>
-                                        <th>Apellido</th>
-                                        <th>Acciones</th>
+                            <tbody>
+                                 {registros.map((dato, key) => (
+                                    <tr key={key}>
+                                        <td>{dato.id}</td>
+                                        <td>{dato.nombre}</td>
+                                        <td>{dato.apellido}</td>
+                                        <td><Button color='primary'>Editar</Button>
+                                            {" "}
+                                            <Button color='danger' onClick={() => handleDelete(dato.id)}>Eliminar</Button></td>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    {this.state.data.map((dato, idx) => (
-                                        <tr key={idx}>
-                                            <td>{dato.id}</td>
-                                            <td>{dato.nombre}</td>
-                                            <td>{dato.apellido}</td>
-                                            <td><Button color='primary' onClick={() => this.mostrarModalActualizar(dato)} >Editar</Button>
-                                                {" "}
-                                                <Button color='danger' onClick={() => this.eliminar(dato)}>Eliminar</Button></td>
-                                        </tr>
-                                    )
-                                    )}
-                                </tbody>
-                            </Table>
-                        </div>
-                    </Container>
-                </div>
+                                ))} 
+                            </tbody>
+                        </Table>
+                    </div>
+                </Container>
+            </div>
 
-                <Modal isOpen={this.state.modalActualizar}>
-                    <ModalHeader>
-                        <div><h3>Editar Registro</h3></div>
-                    </ModalHeader>
-
-                    <ModalBody>
-                        <FormGroup>
-                            <label>Id:</label>
-                            <input className="form-control" readOnly type="text" value={this.state.form.id} />
-                        </FormGroup>
-
-                        <FormGroup>
-                            <label>Personaje: </label>
-                            <input className="form-control" name="nombre" type="text" onChange={this.handleChange} value={this.state.form.nombre} />
-                        </FormGroup>
-
-                        <FormGroup>
-                            <label>Anime:</label>
-                            <input className="form-control" name="apellido" type="text" onChange={this.handleChange} value={this.state.form.apellido} />
-                        </FormGroup>
-                    </ModalBody>
-
-                    <ModalFooter>
-                        <Button color="primary" onClick={() => this.editar(this.state.form)}>Editar</Button>
-                        <Button color="danger" onClick={() => this.cerrarModalActualizar()}>Cancelar</Button>
-                    </ModalFooter>
-                </Modal>
-            </>
-        )
-    }
+        </>
+    );
 }
+
+
 
 export default Detalle;
--- src/features/registros/registrosSlice.js
+++ src/features/registros/registrosSlice.js
@@ -1,19 +1,37 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-const data = [
-    { id: 1, nombre: "Juano", apellido: "Jimenez" },
-    { id: 2, nombre: "Benjamino", apellido: "Nieto" },
-    { id: 3, nombre: "Jhonatano", apellido: "Facete" },
-    { id: 4, nombre: "Roinero", apellido: "Escobar" },
-    { id: 5, nombre: "Joseo", apellido: "Rodriguez" }
+const initialState  = [
+    { id: 1, nombre: "Juan", apellido: "Jimenez" },
+    { id: 2, nombre: "Benjamin", apellido: "Nieto" },
+    { id: 3, nombre: "Jhonatan", apellido: "Facete" },
+    { id: 4, nombre: "Roiner", apellido: "Escobar" },
+    { id: 5, nombre: "Jose", apellido: "Rodriguez" }
 ];
 
-export const registrosSlice = createSlice({
-    name: 'registro',
-    initialState: [data],
+ const registrosSlice = createSlice({
+    name: 'registros',
+    initialState,
     reducers: {
-
+          addRegistro: (state, action) => {
+            state.push(action.payload);
+          },
+          editRegistro: (state, action) => {
+            const { id, nombre, apellido } = action.payload;
+            const foundRegistro = state.find((registro) => registro.id === id);
+            if (foundRegistro) {
+                foundRegistro.nombre = nombre;
+                foundRegistro.apellido = apellido;
+            }
+          },
+          deleteRegistro: (state, action) => {
+            const foundRegistro = state.find((task) => task.id === action.payload);
+            if (foundRegistro) {
+              state.splice(state.indexOf(foundRegistro), 1);
+            }
+          },
     }
 })
 
+
+export const {deleteRegistro} = registrosSlice.actions;
 export default registrosSlice.reducer
