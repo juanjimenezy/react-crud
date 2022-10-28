2d7fa3c98f92fc42a0f45becfa249f28cab9248d
Juan Jimenez <juanjiimenez8@gmail.com>
2022-10-28 13:26:58 -0500

    estilos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { Card, CardHeader,CardBody } from "reactstrap";
 
 import NuevoRegistro from "./NuevoRegistro";
 import Detalle from "./Detalle";
 
 export const Formulario = () => {
     return (
-        <div>
-            <NuevoRegistro/>
-            <Detalle/>
+        <>
+        <div className="my-5 justify-content-center d-flex align-items-center">
+            <Card className="col-md-2">
+                <CardHeader className="text-center">Insertar Registro</CardHeader>
+                <CardBody><NuevoRegistro/></CardBody>
+            </Card>
         </div>
+
+        <div className="my-5 justify-content-center d-flex align-items-center p-2">
+            <Card className="col-md-4">
+                <CardHeader className="text-center">Registros</CardHeader>
+                <CardBody><Detalle/></CardBody>
+                
+            </Card>
+            
+        </div>
+        </>
     )
 }
 
