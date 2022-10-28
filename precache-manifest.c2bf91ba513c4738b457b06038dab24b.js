90e9e98cdf7ee870e5fc7998389bbfa8701631e7
Juan Jimenez <juanjiimenez8@gmail.com>
2022-10-27 23:37:29 -0500

    Updates

---
+++ precache-manifest.c2bf91ba513c4738b457b06038dab24b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "279a864ffd08b7ad5a6a",
+    "url": "/react-crud/static/css/main.e7f2a85b.chunk.css"
+  },
+  {
+    "revision": "279a864ffd08b7ad5a6a",
+    "url": "/react-crud/static/js/main.279a864f.chunk.js"
+  },
+  {
+    "revision": "446dd634eb516a399b9c",
+    "url": "/react-crud/static/css/1.2a3285ff.chunk.css"
+  },
+  {
+    "revision": "446dd634eb516a399b9c",
+    "url": "/react-crud/static/js/1.446dd634.chunk.js"
+  },
+  {
+    "revision": "66e01a8977a9d2cce54a",
+    "url": "/react-crud/static/js/runtime~main.66e01a89.js"
+  },
+  {
+    "revision": "2c8e018e491a88457e2558fb4b8c33cd",
+    "url": "/react-crud/index.html"
+  }
+];
